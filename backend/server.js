e2bb810668355de7ce99385149ef8459d199a000
Your Name <ms.muhammadsalar@gmail.com>
2026-09-04 17:42:02 -0700

    feat : add incident routes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const incidentRoutes = require("./src/routes/incidentRoutes");
 
 const app = express();
 
@@ -7,6 +8,8 @@ app.get("/", (req, res) => {
     message: "Hotel Security System API is running",
   });
 });
+
+app.use("/api/incidents", incidentRoutes);
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
